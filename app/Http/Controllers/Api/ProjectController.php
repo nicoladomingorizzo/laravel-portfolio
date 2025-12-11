73a695b11126247b0df135c5878385c9cd6f7240
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-12-11 12:54:14 +0100

    Add API support with Laravel Sanctum: configure CORS, create ProjectController, and set up personal access tokens migration

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return "sei nell'index api";
+    }
+}
